a73ed7f89e25707e0027eeaefcc198efcb5bb749
Walf <walidfaryabi@hotmail.com>
2016-02-04 14:55:03 +0100

    Added more functionalities.

--- src/network.go
+++ src/network.go
@@ -14,19 +14,19 @@ func checkError(err error, errorpart string) {
 	}
 }
 
-func get_serverAddress(udpType, ipAddress,port string)(net.UDPAddr){
+func get_serverAddress(udpType, ipAddress,port string)(addr *net.UDPAddr){
 	addr, err := net.ResolveUDPAddr(udpType, ipAddress + ":" + port)
 	checkError(err, ": ServerAddress receival")
 	return addr		
 }
 
-func createRemoteSocket(addr net.UDPAddr udpType, ipAddress, port string)(net.UDPConn){
+func createRemoteSocket(addr *net.UDPAddr udpType, ipAddress, port string)(*net.UDPConn){
 	remoteSocket, err := net.DialUDP(udpType, nil, addr)
 	checkError(err, ": Unsuccessfull socket connection")
 	return remoteSocket	 
 }
 
-func createLocalSocket(udpType, ipAddress,port string)(net.UDPConn){
+func createLocalSocket(udpType, ipAddress,port string)(*net.UDPConn){
 	addr, err := net.ResolveUDPAddr(udpType, ipAddress +":" + port)
 	checkError(err,": Unsuccessfull retrieval of address")
 	conn, err := net.ListenUDP(udpType, addr)
@@ -34,15 +34,26 @@ func createLocalSocket(udpType, ipAddress,port string)(net.UDPConn){
 	return conn
 }
 
+func send_message(message string, remoteSocket *net.UDPConn){
+	buffer := make([]byte, 1024)
+	for i:=0; i<len(msg); i++ {
+		buffer[i] = byte(msg[i])
+	}
+	for {
+		_, err := remoteSocket.Write(buffer)
+		checkError(err)
+	}
+}
 
 
-func recv_message(port string, localSocket net.UPDConn)(size, buffer[] byte){
+
+func recv_message(port string, localSocket *net.UPDConn)(size, buffer[] byte){
 	buffer := make([]byte,1024)	
 	size, _ := udpConn.Read(buffer)
 	return size, buffer
 }
 
-for j := 7; j <= 9; j++
+
 
 func read_message(buffer[] byte, size int){
 	for i := 0; i<size; i++{
