151c7a7bea178dd63f20845b52dba72233bae481
Walf <walidfaryabi@hotmail.com>
2016-02-04 14:13:14 +0100

    Added basic socket connection functions.

--- src/network.go
+++ src/network.go
@@ -7,3 +7,30 @@ import (
 )
 
 
+func checkError(err error, errorpart string) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+}
+
+func get_serverAddress(udpType, ipAddress,port string)(net.UDPAddr){
+	addr, err := net.ResolveUDPAddr(udpType, ipAddress + ":" + port)
+	checkError(err, ": ServerAddress receival")
+	return addr		
+}
+
+func createRemoteSocket(addr net.UDPAddr udpType, ipAddress, port string)(net.UDPConn){
+	remoteSocket, err := net.DialUDP(udpType, nil, addr)
+	checkError(err, ": Unsuccessfull socket connection")
+	return remoteSocket	 
+}
+
+func createLocalSocket(udpType, ipAddress,port string)(net.UDPConn){
+	addr, err := net.ResolveUDPAddr(udpType, ipAddress +":" + port)
+	checkError(err,": Unsuccessfull retrieval of address")
+	conn, err := net.ListenUDP(udpType, addr)
+	checkError(err, "Unsuccessfull connection to local socket")
+	return conn
+}
+
