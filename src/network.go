6c87fa625b1f7245ef57b778b40f77430f7e3d3f
Walf <walidfaryabi@hotmail.com>
2016-02-04 14:40:53 +0100

    Added receive message function

--- src/network.go
+++ src/network.go
@@ -34,3 +34,19 @@ func createLocalSocket(udpType, ipAddress,port string)(net.UDPConn){
 	return conn
 }
 
+
+
+func recv_message(port string, localSocket net.UPDConn)(size, buffer[] byte){
+	buffer := make([]byte,1024)	
+	size, _ := udpConn.Read(buffer)
+	return size, buffer
+}
+
+for j := 7; j <= 9; j++
+
+func read_message(buffer[] byte, size int){
+	for i := 0; i<size; i++{
+		s := string(buffer[size])
+		fmt.print(s)
+	}
+}
